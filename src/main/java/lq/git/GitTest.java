4e6b7d54bc210a99a35aec5b4d5c7a1692b00ba5
lingqi <lq_yt5416@163.com>
2022-04-02 19:59:47 +0800

    master commit2

--- src/main/java/lq/git/GitTest.java
+++ src/main/java/lq/git/GitTest.java
@@ -3,5 +3,6 @@ package lq.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git3");
+        System.out.println("Hello master merge");
     }
 }
