8e5ecad6397c21ebaea8b6d700f7b8d43c48f340
lingqi <lq_yt5416@163.com>
2022-04-02 19:48:17 +0800

    third commit

--- src/main/java/lq/git/GitTest.java
+++ src/main/java/lq/git/GitTest.java
@@ -2,6 +2,6 @@ package lq.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
     }
 }
