fba2780159aa96c2e1b3ad261532045f1c23c284
lingqi <lq_yt5416@163.com>
2022-04-02 19:58:54 +0800

    hot-fix commit2

--- src/main/java/lq/git/GitTest.java
+++ src/main/java/lq/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git hot-fix");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git hot-fix");
     }
 }
